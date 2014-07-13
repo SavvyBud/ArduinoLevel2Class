c57f03564b6200d460cc03360de3113acefc7502
SavvyBud <support@savvybud.com>
2014-07-13 07:36:57 -0400

    adding RTTTLib

---
+++ libraries/RTTTLib/RTTTLib.h
@@ -0,0 +1,31 @@
+
+// Ring Tone Transfer Library
+
+// A fun sketch to demonstrate the use of the tone() function written by Brett Hagman 
+// converted into library by Vivek Srivastav for teaching kids
+
+// This plays RTTTL (RingTone Text Transfer Language) songs using the
+// now built-in tone() command in Wiring and Arduino.
+// Written by Brett Hagman
+// http://www.roguerobotics.com/
+
+// To play the output on a small speaker (i.e. 8 Ohms or higher), simply use
+// a 1K Ohm resistor from the output pin to the speaker, and connect the other
+// side of the speaker to ground.
+
+// You can get more RTTTL songs from
+// http://code.google.com/p/rogue-code/wiki/ToneLibraryDocumentation
+
+#define STAR_WARS "StarWars:d=4,o=5,b=45:32p,32f#,32f#,32f#,8b.,8f#.6,32e6,32d#6,32c#6,8b.6,16f#.6,32e6,32d#6,32c#6,8b.6,16f#.6,32e6,32d#6,32e6,8c#.6,32f#,32f#,32f#,8b.,8f#.6,32e6,32d#6,32c#6,8b.6,16f#.6,32e6,32d#6,32c#6,8b.6,16f#.6,32e6,32d#6,32e6,8c#6"
+#define GOOD_BAD "GoodBad:d=4,o=5,b=56:32p,32a#,32d#6,32a#,32d#6,8a#.,16f#.,16g#.,d#,32a#,32d#6,32a#,32d#6,8a#.,16f#.,16g#.,c#6,32a#,32d#6,32a#,32d#6,8a#.,16f#.,32f.,32d#.,c#,32a#,32d#6,32a#,32d#6,8a#.,16g#.,d#"
+#define TOPGUN "TopGun:d=4,o=4,b=31:32p,16c#,16g#,16g#,32f#,32f,32f#,32f,16d#,16d#,32c#,32d#,16f,32d#,32f,16f#,32f,32c#,16f,d#,16c#,16g#,16g#,32f#,32f,32f#,32f,16d#,16d#,32c#,32d#,16f,32d#,32f,16f#,32f,32c#,g#"
+#define MISSION_IMP "MissionImp:d=16,o=6,b=95:32d,32d#,32d,32d#,32d,32d#,32d,32d#,32d,32d,32d#,32e,32f,32f#,32g,g,8p,g,8p,a#,p,c7,p,g,8p,g,8p,f,p,f#,p,g,8p,g,8p,a#,p,c7,p,g,8p,g,8p,f,p,f#,p,a#,g,2d,32p,a#,g,2c#,32p,a#,g,2c,a#5,8c,2p,32p,a#5,g5,2f#,32p,a#5,g5,2f,32p,a#5,g5,2e,d#,8d"
+
+class RTTTLib {
+  public:
+    RTTTLib(int pin);
+    void play(const char *song);
+
+  private:
+    int _pin;
+};
